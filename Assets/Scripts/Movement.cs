b4db1d271eed1dae4a1f0274ad96d36cbc9f941f
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2022-06-07 15:48:09 -0500

    InitialCommit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+    [SerializeField] private GameObject _turbo1 = default;
+    [SerializeField] private GameObject _turbo2 = default;
+    [SerializeField] private bool isturboOn = false;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && !isturboOn )
+        {
+            transform.Translate(Vector3.forward *Time.deltaTime* speed);
+        }
+        if (Input.GetKey(KeyCode.S) && !isturboOn)
+        {
+            transform.Translate(Vector3.back *Time.deltaTime*speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0,1,0 *Time.deltaTime * 0.1f);  
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0,-1,0 *Time.deltaTime * 0.1f);  
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isturboOn = true;
+            transform.Translate(Vector3.forward *Time.deltaTime* speed * 2);
+            _turbo1.SetActive(true);
+            _turbo2.SetActive(true);
+        }
+        else
+        {
+            isturboOn = false;
+            _turbo1.SetActive(false);
+            _turbo2.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private readonly int ahVelocidad = Animator.StringToHash("Velocidad");
+    [SerializeField] private CharacterController _characterController = default;
+    [SerializeField] private bool _isGround = default;
+    [SerializeField] private Vector3 _playerVelocity = default;
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _gravityValue = -9.81f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float velocidad = Input.GetAxis("Vertical");
+        float zaxis = Input.GetAxis("Vertical");
+        float xaxis = Input.GetAxis("Horizontal");
+        _isGround = _characterController.isGrounded;
+        Vector3 move = transform.forward * zaxis;
+        _characterController.transform.Rotate(Vector3.up * xaxis * (100f * Time.deltaTime));
+        if (_isGround && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+        }
+      
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityValue);
+        }
+      
+        _playerVelocity.y += _gravityValue * Time.deltaTime;
+        _characterController.Move(_playerVelocity * Time.deltaTime);
+      
+        if (Input.GetKey(KeyCode.LeftShift))
+        { 
+            anim.SetInteger(ahVelocidad, Mathf.FloorToInt(velocidad) * 2 );
+            _characterController.Move(move * Time.deltaTime * _speed);
+        }
+        else
+        {
+            anim.SetInteger( ahVelocidad, Mathf.FloorToInt(velocidad)); 
+            _characterController.Move(move * Time.deltaTime);
+        }
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Saltar");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        { 
+            anim.Play("DAMAGED"); 
+        }
+    }
+
+}
