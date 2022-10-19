ece3e8c33a83e3a7fa72c987d77305d8ebd89221
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2022-10-19 15:08:21 -0500

    add particle

--- Assets/Scripts/Player/InvisiblePlayer.cs
+++ Assets/Scripts/Player/InvisiblePlayer.cs
@@ -1,29 +1,33 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
 public class InvisiblePlayer : MonoBehaviour
 {
-    [SerializeField] private float transparency = 0f;
      private LayerMask _ignore = default;
      private LayerMask _objective = default;
-     [SerializeField] private Renderer _renderer;
 
-  private void Start()
+     private void Start()
   {
-      _ignore = LayerMask.NameToLayer("OBstacles");
+      _ignore = LayerMask.NameToLayer("Invisible");
       _objective = LayerMask.NameToLayer("AI");
   }
 
   public IEnumerator Invisible()
-   {
-      
-       _renderer.material.color = new Color(1, 1, 1, transparency);
-       gameObject.layer = _ignore;
-        Debug.Log(gameObject.layer);
-        yield return new WaitForSeconds(3f);
-        gameObject.layer = _objective;
-        Debug.Log(gameObject.layer);
-        _renderer.material.color = new Color(1, 1, 1, 1);
-   }
+  {
+      gameObject.layer = _ignore;
+      ChangingLayerChildren(_ignore);
+      yield return new WaitForSeconds(3f);
+      ChangingLayerChildren(_objective);
+      gameObject.layer = _objective;
+  }
+
+  private void ChangingLayerChildren(LayerMask layer)
+  {
+      for (int i = 0; i < gameObject.transform.childCount; i++)
+      {
+          GameObject mesh = gameObject.transform.GetChild(i).gameObject;
+          mesh.layer = layer;
+      }
+  }
+  
 }
