7a8d4db59dbae7af95167619e375f933e0e9dd5f
JesusRuiz02 <79652924+JesusRuiz02@users.noreply.github.com>
2022-11-30 03:57:02 -0600

    Invisiblecanvas

--- Assets/Scripts/Player/InvisiblePlayer.cs
+++ Assets/Scripts/Player/InvisiblePlayer.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InvisiblePlayer : MonoBehaviour
 {
      private LayerMask _ignore = default;
      private LayerMask _objective = default;
+     [SerializeField] private GameObject invisibleCanvas;
+     private float _timer = 0.9f;
+    
 
      private void Start()
   {
@@ -14,9 +18,20 @@ public class InvisiblePlayer : MonoBehaviour
 
   public IEnumerator Invisible()
   {
+      float timer = _timer;
+      invisibleCanvas.SetActive(true);
       gameObject.layer = _ignore;
       ChangingLayerChildren(_ignore);
-      yield return new WaitForSeconds(5f);
+      yield return new WaitForSeconds(2f);
+      while (_timer >= 0.1f)
+      {
+          invisibleCanvas.SetActive(false);
+          yield return new WaitForSeconds(0.1f);
+          invisibleCanvas.SetActive(true);
+          yield return new WaitForSeconds(timer);
+          timer -= 0.1f;
+      }
+      invisibleCanvas.SetActive(false);
       ChangingLayerChildren(_objective);
       gameObject.layer = _objective;
   }
